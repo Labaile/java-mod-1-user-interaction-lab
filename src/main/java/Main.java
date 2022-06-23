97882ace4861625ab41105692bf4a6ffcd2d6c8d
Labaile <Labaile1@gmail.com>
2022-06-23 17:03:24 -0400

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+
+import java.util.Scanner;  //import the scanner class
+
 public class Main {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         // your code here
+        Scanner reader = new Scanner(System.in); //Create a Scanner Object
+        System.out.println("Please enter your age: ");
+        int n = reader.nextInt(); //Read user input
+        System.out.println("You are: " + n); //Output user input
+        reader.close();
+        
+        if (n < 18 && n < 10 ){
+            System.out.println("Congrats! You are under 10!");
+        } else if (n < 18 ) {
+            System.out.println("Congrats! You are under 18!");
+        }
+        else if (n >= 18){
+            System.out.println("Congrats! You are over 18!");
+        } else { 
+            //reader.close();
+            return; }
+
+        
     }
-}
+} 
